0bec586c37d27fb401d12124d3d2e6b5af22c20d
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 15:53:11 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_standard_deviation_calculator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Standard Deviation Calculator by Calculator.iO";
 
 function display_ci_standard_deviation_calculator(){
